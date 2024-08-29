40b5c71df7a2385c6dd0268103b464bd6f0c6aef
sunny_92 <abhinav.paul7@gmail.com>
2024-08-29 23:06:01 +0530

    Python File V3

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 def greet_user(name):
-    print(f"Hello, {name}! Welcome to Jenkins automation. My Updated Version 2")
+    print(f"Hello, {name}! Welcome to Jenkins automation. My Updated Version 3")
 
 def calculate_sum(a, b):
     return a + b
